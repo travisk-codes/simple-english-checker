6457a669cff78473f3a7c6a5f6a5f545c20a2739
Travis Kohlbeck <me+git@travisk.info>
2019-11-17 23:41:30 -0500

    fixes layout

--- src/App.js
+++ src/App.js
@@ -7,62 +7,78 @@ import { makeStyles } from '@material-ui/core/styles'
 import words from './words'
 import './App.css'
 
-
 const useStyles = makeStyles(theme => ({
-  button: {
-    width: '16em'
-  },
-  textarea: {
-    width: "100%"
-  },
-  titleAndBtn: {
-    display: "flex",
-    justifyContent: "space-between",
-    flexWrap: "wrap",
-    height: "2em",
-    marginTop: "32px"
-  },
-  app: {
-    textAlign: "center",
-    width: "90%",
-    maxWidth: "38em",
-    margin: "0 auto"
-  },
-  title: {
-    fontSize: "1.5em"
-  },
-  hidden: {
-    display: "none"
-  },
-  nonEditMode: {
-    "& *": {
-      borderStyle: "none",
-      color: "rgba(0, 0, 0, 0.87)",
-    },
-    "& label": {
-      display: "none"
-    },
-  }
+	button: {
+		width: '14em',
+	},
+	textarea: {
+		marginTop: '24px',
+		width: '100%',
+	},
+	titleAndBtn: {
+		display: 'flex',
+		flexWrap: 'wrap',
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+		marginTop: '24px',
+		height: '36px',
+		alignItems: 'center',
+	},
+	app: {
+		textAlign: 'center',
+		width: '90%',
+		maxWidth: '38em',
+		margin: '0 auto',
+		display: 'flex',
+		flexDirection: 'column-reverse',
+		flexWrap: 'wrap',
+		justifyContent: 'space-between',
+	},
+	title: {
+		fontSize: '1.5em',
+	},
+	hidden: {
+		display: 'none',
+	},
+	nonEditMode: {
+		'& *': {
+			borderStyle: 'none',
+			color: 'rgba(0, 0, 0, 0.87)',
+		},
+		'& label': {
+			display: 'none',
+		},
+	},
 }))
 
 function colorWords(text) {
 	let word_matches = []
-  text = text.split(' ')
+	text = text.split(' ')
 	text.forEach(word => {
 		word_matches.forEach(match => {
-      if (match.word === word) return
+			if (match.word === word) return
 		})
-    let pos = words.indexOf(word.toLowerCase().replace(/[^a-zA-Z0-9]/, ''))
+		let pos = words.indexOf(
+			word.toLowerCase().replace(/[^a-zA-Z0-9]/, '')
+		)
 		word_matches.push({
 			word,
-			pos
+			pos,
 		})
-  })
+	})
 	let spans = word_matches.map(match => {
-    if (match.pos === -1) match.pos = 10000
-    let color = `hsla(${match.pos / 10000 * 18}, 100%, 50%, ${match.pos / 10000})`
-		return (<><span style={{backgroundColor: color}}>{match.word}</span><span>&nbsp;</span></>)
-  })
+		if (match.pos === -1) match.pos = 10000
+		let color = `hsla(${(match.pos / 10000) *
+			18}, 100%, 50%, ${match.pos / 10000})`
+		return (
+			<>
+				<span style={{ backgroundColor: color }}>
+					{match.word}
+				</span>
+				<span>&nbsp;</span>
+			</>
+		)
+	})
 	return spans || <span>error</span>
 }
 
@@ -75,57 +91,61 @@ function App() {
 		let visibility = !user_text ? classes.hidden : null
 		let label = editMode ? 'how simple is it?' : 'edit text'
 		return (
-      <Button
-        className={`${visibility} ${classes.button}`}
-        variant="outlined"
-				color="primary"
+			<Button
+				className={`${visibility} ${classes.button}`}
+				variant='outlined'
+				color='secondary'
 				onClick={() => setEditMode(!editMode)}
-      >
-        {label}
-      </Button>
-    );
+			>
+				{label}
+			</Button>
+		)
 	}
 
 	function renderTextarea() {
 		let element, klasses
-    if (editMode) {
-      element = 'input'
-      klasses = classes.textarea
+		if (editMode) {
+			element = 'input'
+			klasses = classes.textarea
 		} else {
-      element = () => colorWords(user_text)
-      klasses = `${classes.textarea} ${classes.nonEditMode}`
-    }
+			element = () => colorWords(user_text)
+			klasses = `${classes.textarea} ${classes.nonEditMode}`
+		}
 		return (
-      <TextField
-        className={klasses}
-        label="Place prose here"
-        multiline
-        placeholder="The quick brown fox jumps over the lazy platypus."
-        margin="normal"
-        variant="outlined"
+			<TextField
+				color='secondary'
+				className={klasses}
+				label='Place prose here'
+				multiline
+				placeholder='The quick brown fox jumps over the lazy platypus.'
+				margin='normal'
+				variant='outlined'
 				value={user_text}
 				disabled={!editMode}
-        onChange={e => setUserText(e.target.value)}
-        InputProps={{
-          inputComponent: element,
-          style: {
-            display: 'flex',
-            flexWrap: 'wrap',
-          },
-        }}
-      />
-    )
+				onChange={e => setUserText(e.target.value)}
+				InputProps={{
+					tabIndex: '2',
+					inputComponent: element,
+					style: {
+						display: 'flex',
+						flexWrap: 'wrap',
+					},
+				}}
+			/>
+		)
 	}
 
 	return (
 		<div className={classes.app}>
+			{renderTextarea()}
 			<div className={classes.titleAndBtn}>
-  <span className={classes.title}>{colorWords('Simple English Checker')}</span>
+				<span className={classes.title}>
+					{colorWords('Simple English Checker')}
+				</span>
 				{renderButton()}
 			</div>
-			{renderTextarea()}
 		</div>
-	);
+	)
 }
 
-export default App;
+export default App
